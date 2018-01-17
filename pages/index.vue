f8df8352dfa7e93ec229ddd3d240b57cf63ee1be
rajabiseraji <rajabiseraji@outlook.com>
2018-01-17 22:02:40 +0330

    feat(store): adds homepage store and AJAX

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,14 @@
   <section class="container mb-5">
     <div>
       <search-container title="جستجوی فلان و بیسار و اینا"></search-container>
-      <static-banner title="بروسلی دانلود کن" subTitle="زیرنویس" img="~/assets/img/avatar.png" color="#DAA521"></static-banner>
+      <static-banner :title="homepageData.staticBannerTitle" 
+                     :subTitle="homepageData.staticBannerSubTitle" 
+                     :img="homepageData.staticBanner" 
+                     :color="homepageData.staticBannerBackColor"
+                     :textColor="homepageData.staticBannerTextColor">
+      </static-banner>
       <movie-carousel title="داغ‌ترین‌ها"></movie-carousel>
+      {{homepageData}}
     </div>
   </section>
 </template>
@@ -12,8 +18,17 @@
 import SearchContainer from '~/components/MainPage/SearchContainer.vue';
 import StaticBanner from '~/components/MainPage/StaticBanner.vue';
 import MovieCarousel from '~/components/MainPage/MovieCarousel.vue';
+import {mapGetters} from 'vuex';
 
 export default {
+  computed: {
+    ...mapGetters({
+      homepageData: 'homepage/getState'
+    })
+  },
+  async fetch ({store, params}) {
+    await store.dispatch('homepage/getHomepageData');
+  },
   components: {
     SearchContainer,
     StaticBanner,
