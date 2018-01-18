dc1263cef48a90b330c6d89ae8a75068e6788b0f
rajabiseraji <rajabiseraji@outlook.com>
2018-01-18 14:19:54 +0330

    feat(movies): adds movie page

--- pages/movie/_id.vue
+++ pages/movie/_id.vue
@@ -1,47 +1,172 @@
 <template>
-  <div>Hello</div>
+<div class="text-right">
+    <div>
+        <div class="movie-header">
+            <div class="container">
+                <h3 class="movie-title">
+                    {{movieData.Title}}
+                </h3>
+                <div class="genres">
+                    <button v-for="genre in movieData.genres" 
+                            :key="genre" 
+                            class="btn-genre btn-sm">
+                            {{genre}}
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="movie-background">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-2 ml-5">
+                        <img class="movie-img movie-img-border" :src="movieData.Poster" />
+                    </div>
+                    <div class="col-sm-12 col-md-7 mr-5">
+                        <button class="btn imdb-rate ml-1">
+                            {{movieData.imdbRating}} از ۱۰ با {{movieData.imdbVotes}} رای
+                        </button>
+                        <button class="btn imdb ml-1">
+                            <a :href="`http://i.iranfilm.co/title/${movieData.imdbID}/`">
+                                IMDB
+                            </a>
+                        </button>
+                        <button class="btn subtitle">Subscene</button>
+                        <div class="mt-3">
+                            <span class="pl-3 movie-year">
+                                {{movieData.Released}}
+                            </span>
+                            <span class="pl-3 movie-duration">
+                                {{movieData.Runtime}}
+                            </span>
+                            <span class="pl-3 movie-lang">
+                                {{movieData.Language}}
+                            </span>
+                            <span class="px-2 py-2 badge badge-secondary bg-transparent" :style="{border: '1px solid white'}">
+                                {{movieData.Rated}}
+                            </span>
+                            <span class="pl-3 pr-3 movie-country">
+                                 محصول کشور {{movieData.Country}}
+                            </span>
+                        </div>
+
+                        <hr class="hr"/>
+                        <div class="movie-detail">
+                            <h5 class="movie-title">{{movieData.TranslatedTitle}}</h5>
+                            <div class="ml-5 movie-summary">{{movieData.TranslatedPlot}}</div>
+                        </div>
+                        <hr class="hr"/>
+                        <div class="movie-cast">
+                            <div class="director my-2">
+                                کارگردان: {{movieData.Director}}
+                            </div>
+                            <div class="writer my-2">
+                                نویسنده: {{movieData.Writer}}
+                            </div>
+                            <div class="stars my-2">
+                                بازیگران: {{movieData.Actors}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <download-tab :downloadRate="movieData.downloadRate"
+                  :totalVotes="movieData.totalVotes"
+                  :positiveVotes="movieData.positiveVotes"
+                  :qualities="movieData.availableDownloadQualities"
+                  :resolutions="movieData.availableDownloadResolutions"  
+    ></download-tab>
+</div>
 </template>
 
 <script>
-
+import DownloadTab from '~/components/MoviePage/DownloadTab.vue';
+import { mapGetters } from'vuex';
 export default {
+    async fetch({store, params}) {
+        const movieID = params.id;
+        if(movieID != 'bootstrap.css.map'){
+            await store.dispatch('movie/getMovieData', movieID);
+            await store.dispatch('movie/getComments', movieID);
+        }
+    },
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+        ...mapGetters({
+            movieData: 'movie/movieData',
+            comments: 'movie/comments',
+        })
+    },
+    components: {
+        DownloadTab,
+    }
 
 }
 </script>
-    data() {
-        return {
-            title: '',
-            rating: 0,
-            genres: [],
-            year: '',
-            voteCount: 200,
-            length: '',
-            languages: [],
-            countries: [],
-            summary: '',
-            directors: [],
-            writers: [],
-            stars: [],
-            downloadRate: 8, //out of ten
-            totalVotes: 10,
-            positiveVotes: 5,
-            availableDownloadLanguages: [],
-            availableDownloadQualities: [],
-            availableDownloadResolutions: [],
-            comments: [
-                {
-                    user: '',
-                    date: '',
-                    positive: true,
-                    upVotesForThisComment: 10,
-                    downVotesForThisComment: 20,
-                    directorshipScore: 8,
-                    actorshipScore: 5,
-                    writingScore: 10,
-                }
-            ]
-        }
+<style scoped>
+    a, a:hover, a:active {
+        text-decoration: none;
+        color: inherit;
     }
-<style>
 
+    .movie-header{
+        background-color: #f19a27;
+        padding: 30px;
+        color: #ffffff;
+    }
+
+    .btn-genre{
+        background-color: #e9d8d7;
+        color: black;
+        border: solid 2px grey;
+        margin: 5px 0 5px 5px;
+        border-radius: 5px;
+    }
+
+    .movie-img{
+        width: 200px;
+        display: inline;
+        position: relative;
+        top: -45px;
+    }
+
+    .movie-img-border{
+        border: solid 8px #f19a27;
+    }
+
+    .movie-background{
+        padding: 15px;
+        background-color: black;
+        color: #FFFFFF;
+    }
+
+    .imdb-rate{
+        background-color: transparent;
+        color: #ffffff;
+        border: solid 1px white;
+    }
+
+    .imdb{
+        background-color: #e4b42d;
+        color: black;
+    }
+
+    .subtitle{
+        background-color:#20516d;
+        color: white;
+    }
+
+    .rated{
+        border: solid 1px white;
+        border-radius: 5px;
+    }
+
+    .hr{
+        border-color: #747475;
+    }
 </style>
